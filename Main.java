fedeec37e213b648d3e5f2343da8a0510de9f21d
thuong1511 <nguyenthithuong15112005@gmail.com>
2025-09-18 20:49:56 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package pkg3;
@@ -31,3 +30,4 @@ public class Main {
 	}
 
 }
+
